acf350b9a5d024dfc83be4f010ac75bb42848d92
yatw <yatw@uci.edu>
2018-12-29 12:58:23 -0800

    Included sample map

---
+++ Assets/Maps/Extra_Unfinished4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Extra_Unfinished4" tilewidth="16" tileheight="16" tilecount="352" columns="22">
+ <image source="../Downloads/Extra_Unfinished4.png" width="360" height="266"/>
+</tileset>
---
+++ Assets/Maps/ground_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="ground_tiles" tilewidth="16" tileheight="16" tilecount="1806" columns="42">
+ <image source="../Art/ground_tiles.png" width="672" height="701"/>
+</tileset>
