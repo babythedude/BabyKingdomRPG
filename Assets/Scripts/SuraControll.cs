6c3eab38b744c7637a73fd89c02d4b50bba2966f
yatw <yatw@uci.edu>
2018-12-30 22:37:03 -0800

    Included Sura Character and movement

---
+++ Assets/Scripts/SuraControll.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuraControll : MonoBehaviour
+{
+	
+	public float moveSpeed;
+	private Animator anim;
+	
+	private bool SuraMoving;
+	private Vector2 lastMoveX;
+	private Vector2 lastMoveY;
+	
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		SuraMoving = false;
+		
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") <- 0.5f)
+		{
+				transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+				SuraMoving = true;
+				lastMoveX = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+		}	
+		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") <- 0.5f)
+		{
+				transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime,0f));
+				SuraMoving = true;
+				lastMoveY = new Vector2(0f,Input.GetAxisRaw("Vertical"));
+		}
+		
+		anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+		anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+		anim.SetBool("SuraMoving", SuraMoving);
+		anim.SetFloat("LastMoveX", lastMoveX.x);
+		anim.SetFloat("LastMoveY", lastMoveY.y);
+		
+        
+    }
+}
